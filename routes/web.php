9022ac52f2c785bfe62c6f5419c1f9d7e0d3b960
oktaharis <oktaharissutanto@smkwikrama.sch.id>
2023-12-19 21:04:30 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-    return view('Menambahkan Route hello');
-});
+ Route::get('/hello', function(){
+    return 'Hellow World!';
+ });
