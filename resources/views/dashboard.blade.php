b9849a0088255c4e3ebfacd606be438d3be33148
oktaharis <oktaharissutanto@smkwikrama.sch.id>
2023-12-27 15:28:54 +0700

    penambahanFooter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,357 @@
 @extends('layouts.template')
+
 @section('content')
+
+<div class="div">
+    <img
+      loading="lazy"
+      srcset="..."
+      class="img"
+    />
+    <div class="div-2"></div>
+    <div class="div-3">
+      <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/3e7465b2d782a74a2a82bd4336c49f50eff9e2a6ca6ca5710fb6ef22fba5a845?"
+        class="img-2"
+      />
+      <div class="div-4">Kategori Pilihan</div>
+      <div class="div-5">
+        <img
+          loading="lazy"
+          srcset="..."
+          class="img-3"
+        />
+        <div class="div-6"></div>
+        <div class="div-7"></div>
+        <div class="div-8"></div>
+        <div class="div-9"></div>
+        <div class="div-10"></div>
+      </div>
+      <div class="div-11">
+        <div class="div-12">
+          <img
+            loading="lazy"
+            srcset="..."
+            class="img-4"
+          />
+          <div class="div-13">MACKBOOK</div>
+        </div>
+        <div class="div-14">
+          <img
+            loading="lazy"
+            srcset="..."
+            class="img-5"
+          />
+          <div class="div-15">ROG</div>
+        </div>
+        <div class="div-16">
+          <img
+            loading="lazy"
+            srcset="..."
+            class="img-6"
+          />
+          <div class="div-17">ACER</div>
+        </div>
+        <div class="div-18">
+          <img
+            loading="lazy"
+            srcset="..."
+            class="img-7"
+          />
+          <div class="div-19">HP</div>
+        </div>
+        <div class="div-20">
+          <img
+            loading="lazy"
+            srcset="..."
+            class="img-8"
+          />
+          <div class="div-21">LENOVO</div>
+        </div>
+        <div class="div-22">
+          <img
+            loading="lazy"
+            srcset="..."
+            class="img-9"
+          />
+          <div class="div-23">MSI</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <style>
+    .div {
+      display: flex;
+      flex-direction: column;
+    }
+    .img {
+      aspect-ratio: 3.51;
+      object-fit: contain;
+      object-position: center;
+      width: 100%;
+      border-radius: 15px;
+      overflow: hidden;
+    }
+    
+    .div-2 {
+      background-color: #4477ce;
+      width: 989px;
+      max-width: 100%;
+      height: 6px;
+      margin: 13px 28px 0;
+    }
+    
+    .div-3 {
+      disply: flex;
+      flex-direction: column;
+      fill: #0c134f;
+      overflow: hidden;
+      position: relative;
+      display: flex;
+      min-height: 298px;
+      margin: 11px 25px 0;
+      padding: 28px 36px 28px 31px;
+    }
+    
+    .img-2 {
+      position: absolute;
+      inset: 0;
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+    .div-4 {
+      position: relative;
+      color: #fff;
+      font: 600 15px Inter, sans-serif;
+    }
+    
+    .div-5 {
+      position: relative;
+      display: flex;
+      margin-top: 28px;
+      padding-right: 12px;
+      justify-content: space-between;
+      gap: 20px;
+    }
+    
+    .img-3 {
+      aspect-ratio: 1.25;
+      object-fit: contain;
+      object-position: center;
+      width: 128px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-6 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      width: 126px;
+      height: 102px;
+      flex-direction: column;
+    }
+    .div-7 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      width: 126px;
+      height: 102px;
+      flex-direction: column;
+    }
+    .div-8 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      width: 125px;
+      height: 102px;
+      flex-direction: column;
+    }
+    .div-9 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      width: 126px;
+      height: 102px;
+      flex-direction: column;
+    }
+    .div-10 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      width: 126px;
+      height: 102px;
+      flex-direction: column;
+    }
+    .div-11 {
+      position: relative;
+      display: flex;
+      margin-top: 57px;
+      padding-right: 18px;
+      justify-content: space-between;
+      gap: 20px;
+    }
+    
+    .div-12 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      justify-content: space-between;
+      gap: 0px;
+      padding: 4px 5px;
+    }
+    .img-4 {
+      aspect-ratio: 0.91;
+      object-fit: contain;
+      object-position: center;
+      width: 30px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-13 {
+      color: #fff;
+      align-self: center;
+      flex-grow: 1;
+      white-space: nowrap;
+      margin: auto 0;
+      font: 600 15px Inter, sans-serif;
+    }
+    
+    .div-14 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      justify-content: space-between;
+      gap: 5px;
+      padding: 6px 37px 0 3px;
+    }
+    
+    .img-5 {
+      aspect-ratio: 1.66;
+      object-fit: contain;
+      object-position: center;
+      width: 58px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-15 {
+      color: #fff;
+      align-self: center;
+      flex-grow: 1;
+      white-space: nowrap;
+      margin: auto 0;
+      font: 600 15px Inter, sans-serif;
+    }
+    
+    .div-16 {
+      border-radius: 15px;
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      justify-content: space-between;
+      gap: 3px;
+      padding: 8px 7px;
+    }
+    
+    .img-6 {
+      aspect-ratio: 2.16;
+      object-fit: contain;
+      object-position: center;
+      width: 54px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-17 {
+      color: #fff;
+      align-self: center;
+      flex-grow: 1;
+      white-space: nowrap;
+      margin: auto 0;
+      font: 600 15px Inter, sans-serif;
+    }
+    
+    .div-18 {
+      border-radius: 15px;
+      box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      justify-content: space-between;
+      gap: 6px;
+      padding: 1px 60px 1px 4px;
+    }
+    
+    .img-7 {
+      aspect-ratio: 1.18;
+      object-fit: contain;
+      object-position: center;
+      width: 46px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-19 {
+      color: #fff;
+      align-self: center;
+      flex-grow: 1;
+      white-space: nowrap;
+      margin: auto 0;
+      font: 600 15px Inter, sans-serif;
+    }
+    
+    .div-20 {
+      border-radius: 15px;
+      box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      justify-content: space-between;
+      gap: 4px;
+      padding: 3px 13px 3px 3px;
+    }
+    .img-8 {
+      aspect-ratio: 1.36;
+      object-fit: contain;
+      object-position: center;
+      width: 49px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-21 {
+      color: #fff;
+      align-self: center;
+      flex-grow: 1;
+      white-space: nowrap;
+      margin: auto 0;
+      font: 600 15px Inter, sans-serif;
+    }
+    
+    .div-22 {
+      border-radius: 15px;
+      box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+      background-color: rgba(133, 133, 133, 0.24);
+      display: flex;
+      justify-content: space-between;
+      gap: 2px;
+      padding: 5px 7px;
+    }
+    
+    .img-9 {
+      aspect-ratio: 1.41;
+      object-fit: contain;
+      object-position: center;
+      width: 45px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-23 {
+      color: #fff;
+      align-self: center;
+      flex-grow: 1;
+      white-space: nowrap;
+      margin: auto 0;
+      font: 600 15px Inter, sans-serif;
+    }
+    
+  </style>
+  
+  
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{ asset('img/logotranparan.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <link rel="shortcut icon" href="favicon.ico" type="{{ asset('img/logotranparan.png') }}">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-    <title>LapCug. TECH</title>
-</head>
-<style>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <style>
+        :root {
+        --primary: #bd8ceb;
+        --bg: #0C134F;
+        --hover: #3498db;
+        }
         * {
             font-family: Pangram-ExtraLight;
         }
@@ -19,52 +25,209 @@
             font-family: Pangram-ExtraLight;
             src: url({{ asset('PangramSans-FreeForPersonalUse/Pangram-FullFamily-FreeForPersonalUse/Pangram-Bold.otf') }});
         }
-    body{
-        margin: 0;
-        padding: 0
-    }
 
-    .nav{
-        display: flex;
-        align-items: center;
-        width: 1300px;
-        height: 100px;
-        background: #0C134F;
-    }
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
 
-    .nav img{
-        width: 100px;
-    }
+        body {
+            margin: 0;
+            padding: 0;
+            line-height: 1.5;
+            font-family: 'Poppins', sans-serif;
+            min-height: 1000px;
 
-    .nav H4{
-        color:#03AEC6;
-    }
+        }
+        
 
-    .nav input{
-        width: 600px;
-    }
+        .nav {
+            display: flex;
+            align-items: center;
+            width: 100%;
+            height: 100px;
+            background: var(--bg);
+            color: #03AEC6;
+        }
 
-    .nav i{
-        font-size: 30px;
-        margin-left: 30px;
-        width: 50px;
-        padding-left: 10px;
-        padding-top: 3px;
-        background: #5C469C;
-        border-radius: 5px;
-        height: 50px;
-    }
-</style>
+        .nav img {
+            width: 100px;
+        }
+
+        .nav h4 {
+            color: #03AEC6;
+        }
+
+        .nav input {
+            width: 600px;
+        }
+
+        .nav i {
+            font-size: 30px;
+            margin-left: 30px;
+            width: 50px;
+            padding-left: 10px;
+            padding-top: 3px;
+            background: #5C469C;
+            border-radius: 5px;
+            height: 50px;
+            color: white;
+        }
+
+        /* FOOTER */
+        .container {
+            max-width: 1170px;
+            margin: auto;
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        ul {
+            list-style: none;
+        }
+
+        .footer {
+            background-color: var(--bg);
+            padding: 70px 0;
+            position: relative;
+            
+        }
+
+        .footer-col {
+            width: 25%;
+            padding: 0 15px;
+        }
+
+        .footer-col h4 {
+            font-size: 18px;
+            color: #ffffff;
+            text-transform: capitalize;
+            margin-bottom: 35px;
+            font-weight: 500;
+            position: relative;
+        }
+
+        .footer-col h4::before {
+            content: '';
+            position: absolute;
+            left: 0;
+            bottom: -10px;
+            background-color: var(--primary);
+            height: 2px;
+            box-sizing: border-box;
+            width: 50px;
+        }
+
+        .footer-col ul li:not(:last-child) {
+            margin-bottom: 10px;
+        }
+
+        .footer-col ul li a {
+            font-size: 16px;
+            text-transform: capitalize;
+            color: #ffffff;
+            text-decoration: none;
+            font-weight: 300;
+            color: #bbbbbb;
+            display: block;
+            transition: all 0.3s ease;
+        }
+
+        .footer-col ul li a:hover {
+            color: #ffffff;
+            padding-left: 8px;
+        }
+
+        .footer-col .social-links a {
+            display: inline-block;
+            height: 40px;
+            width: 40px;
+            background-color: rgba(255, 255, 255, 0.2);
+            margin: 0 10px 10px 0;
+            text-align: center;
+            line-height: 40px;
+            border-radius: 50%;
+            color: #ffffff;
+            transition: all 0.5s ease;
+        }
+
+        .footer-col .social-links a:hover {
+            color: #24262b;
+            background-color: #ffffff;
+        }
+
+        /*responsive*/
+        @media(max-width: 767px) {
+            .footer-col {
+                width: 50%;
+                margin-bottom: 30px;
+            }
+        }
+
+        @media(max-width: 574px) {
+            .footer-col {
+                width: 100%;
+            }
+        }
+    </style>
+    <title>LapCug. TECH</title>
+</head>
+
 <body>
     <div class="nav">
         <img src="{{ asset('img/logotranparan.png') }}" alt="">
         <h4>LapCug TECH</h4>
         <label for="search" style="color: white; margin:20px;">Cari Produk</label>
         <input type="text" name="search" id="search" placeholder="Cari Produk Disini...">
-        <i class="bi bi-cart" style="color: white;"></i>
+        <i class="bi bi-cart"></i>
 
         <button type="button" style="margin-left: 30px;" class="btn btn-outline-info">Login</button>
         <button type="button" style="margin-left: 10px;" class="btn btn-warning">Register</button>
     </div>
+    <div class="container">
+        {{-- bagian dinamis yang akan berubah tiap pagenya, harus di isi di file yang extends ke template ini --}}
+        @yield('content')
+    </div>
+
+    {{-- FOOTER --}}
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Tentang LapCug .TECH</h4>
+                    <ul>
+                        <li><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa incidunt earum tenetur porro, eligendi ullam repellendus dolorum tempora. Quos expedita et odio nemo similique voluptates porro assumenda atque unde exercitationem eius sapiente pariatur reiciendis, dolore repellat magni, iusto voluptate hic quas itaque. Veritatis, quod eveniet dicta velit voluptas dolorum reprehenderit?</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Jual</h4>
+                    <ul>
+                        <li><a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, repellat voluptates aliquam est voluptatum sint?</a></li>
+                    </ul>
+                    <br><br>
+                     <ul>
+                        <li><a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam quo aperiam, aliquam nihil, repellat nobis quis veniam officiis excepturi iste perferendis, est dolorum tempora neque?</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Ikuti Kami</h4>
+                    <ul>
+                        <li><a href="#">Di sini gak tau apa</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>follow us</h4>
+                    <div class="social-links">
+                    <img src="{{ asset('img/logotranparan.png') }}" alt="logoLapCug" style="max-width: 100%; max-height: 20rem;">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
+
 </html>
